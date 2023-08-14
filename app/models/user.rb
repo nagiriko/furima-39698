690339008a4bbd6b49e1fc027a0e079add75b61e
nagiriko <r.nagihashi@gmail.com>
2023-08-15 00:51:50 +0900

    ユーザー登録及びログイン完了

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
+  validates :nickname,                 presence: true
+  validates :email,                    presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :encrypted_password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[0-9])/i }
+  validates :password_confirmation,    presence: true
+  validates_confirmation_of :password
+  validates :last_name,                 presence: true, format: { with: /\A[ぁ-んァ-ン一-龯々]+\z/ }
+  validates :first_name,                presence: true, format: { with: /\A[ぁ-んァ-ン一-龯々]+\z/ }
+  validates :last_name_kana,            presence: true, format: { with: /\A[ァ-ンー－]+\z/ }
+  validates :first_name_kana,           presence: true, format: { with: /\A[ァ-ンー－]+\z/ }
+  validates :birthdate,                 presence: true
+  has_many :items
+  has_many :orders
+end
