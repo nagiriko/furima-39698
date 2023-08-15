9435edade0048b56439189034492129c28f88e48
nagiriko <r.nagihashi@gmail.com>
2023-08-15 15:22:00 +0900

    プルリクエストの修正：models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :last_name,                 presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
-  validates :first_name,                presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :last_name,                 presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ーヶ]+\z/ }
+  validates :first_name,                presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ーヶ]+\z/ }
   validates :last_name_kana,            presence: true, format: { with: /\A[ァ-ンー－]+\z/ }
   validates :first_name_kana,           presence: true, format: { with: /\A[ァ-ンー－]+\z/ }
   validates :birthdate,                 presence: true
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 end
