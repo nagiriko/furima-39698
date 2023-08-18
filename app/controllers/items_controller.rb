b97304d65e35433f357094d87e29de6cdaf12c6c
nagiriko <r.nagihashi@gmail.com>
2023-08-18 14:43:08 +0900

    プルリクエストの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,8 +20,12 @@ def create
 end
 
 def destroy
-  @item.destroy
-  redirect_to root_path
+  if current_user == @item.user
+    @item.destroy
+    redirect_to root_path
+  else
+    redirect_to root_path
+  end
 end
 
 def show
