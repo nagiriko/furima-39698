29760523f4812d381b3f1670be970b7ee6c75a1e
nagiriko <r.nagihashi@gmail.com>
2023-08-28 16:24:54 +0900

    エラーメッセージを日本語に修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,11 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_duration_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :condition_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :shipping_fee_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :shipping_duration_id, numericality: { other_than: 1 , message: "を選択してください"}
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true  }
   validates :image, presence: true 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,7 +5,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :prefecture_id, numericality:  { other_than: 1 , message: "can't be blank"}
+    validates :prefecture_id, numericality:  { other_than: 1 , message: "を選択してください"}
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
